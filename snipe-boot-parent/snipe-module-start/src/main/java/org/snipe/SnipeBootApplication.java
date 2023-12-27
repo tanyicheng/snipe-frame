60acffecba229e50414ebb874a0d3489cc8ad325
yicheng.tan <yicheng.tan@seraphim-energy.com>
2023-12-27 16:32:08 +0800

    feat: 新增 swagger

--- snipe-boot-parent/snipe-module-start/src/main/java/org/snipe/SnipeBootApplication.java
+++ snipe-boot-parent/snipe-module-start/src/main/java/org/snipe/SnipeBootApplication.java
@@ -32,7 +32,7 @@ public class SnipeBootApplication extends SpringBootServletInitializer {
                 "Application Jeecg-Boot is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
                 "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
-//                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
                 "----------------------------------------------------------");
     }
 
