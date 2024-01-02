fd75f1e0adadbbf127891004070fec4ea62c1e53
yicheng.tan <yicheng.tan@seraphim-energy.com>
2024-01-02 13:06:35 +0800

    feat: swagger api测试

--- snipe-boot-parent/snipe-module-start/src/main/java/org/snipe/modules/demo/HelloWorldController.java
+++ snipe-boot-parent/snipe-module-start/src/main/java/org/snipe/modules/demo/HelloWorldController.java
@@ -1,15 +1,22 @@
 package org.snipe.modules.demo;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.snipe.modules.system.SnipeEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "hello")
 @RestController
 public class HelloWorldController {
+
+    @ApiOperation(value = "hello", notes = "hello 接口测试")
     @GetMapping("/hello")
     public String hello() {
 
         SnipeEntity se = new SnipeEntity();
+        System.out.println(111111111);
         return "Hello, World!";
     }
 }
